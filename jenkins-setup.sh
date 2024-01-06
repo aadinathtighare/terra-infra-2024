991fbd98d098d1bcc447d247dba0a67b0996bb80
devops <118253285+aadinathtighare@users.noreply.github.com>
2024-01-06 18:27:11 +0530

    Create jenkins-setup.sh

---
+++ jenkins-setup.sh
@@ -0,0 +1,7 @@
+yum install git maven jenkins -y
+yum install java-1.8.0-openjdk -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+amazon-linux-extras install epel -y
+amazon-linux-extras install java-openjdk11 -y
+systemctl restart jenkins
